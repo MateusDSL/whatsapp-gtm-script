784848d4a67e9b06c20d0d5d7b19c3cee860a6ca
MateusDSL <mateuslima355@outlook.com>
2026-02-26 15:58:23 -0300

    Update Google Script Web App URL in whatsapp-button.js

--- whatsapp-button.js
+++ whatsapp-button.js
@@ -3,7 +3,7 @@
     console.log("WhatsApp Widget: Iniciando script..."); // Log de debug
 
     // --- CONFIGURAÇÕES ---
-    const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxxFYzWMDVTdEWtfSa-WCuqMuRwQJFnqS1za2ivkVNffz-NbMZ2r1V5BSGUV5AxpdZVHw/exec";
+    const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzGP-oerOAEq8xcMhbiFWWLLivaZF-QKFmfeh2PvRlWdkhG4yf-Od8xQGfmHg4FJUYkPQ/exec";
     const SUPABASE_URL = 'https://hdqrcmxiyanhqligzrpv.supabase.co';
     const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkcXJjbXhpeWFuaHFsaWd6cnB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExMzE0NTYsImV4cCI6MjA2NjcwNzQ1Nn0.efbHgndvp-SHT1TjbjjbXht76Y_fUcRxAPiqeGmCpZU';
     const CSS_URL = "https://mateusdsl.github.io/whatsapp-gtm-script/whatsapp-button.css";
